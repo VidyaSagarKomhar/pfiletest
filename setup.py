7156aaf59f467f916ba653d5cae4f6ac897f7a58
VidyaSagarKomhar <123475184+VidyaSagarKomhar@users.noreply.github.com>
2023-03-01 16:13:03 +0530

    setup.py
    version update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pfiletest',
-    version='0.1.0',    
+    version='0.1.1',    
     description='A example Python package',
     url='https://github.com/VidyaSagarKomhar/pfiletest',
     author='Vidya Sagar',
