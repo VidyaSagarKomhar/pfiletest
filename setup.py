9efc2995b21f55a05d1ce56357c348ba66c1d28d
VidyaSagarKomhar <123475184+VidyaSagarKomhar@users.noreply.github.com>
2023-03-01 12:02:52 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='pfiletest',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/VidyaSagarKomhar/pfiletest',
+    author='Vidya Sagar',
+    author_email='vidya.sagar@komhar.com',
+    license='BSD 2-clause',
+    packages=['pfiletest'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Komhar',  
+        'Operating System :: OS Independent',        
+        'Programming Language :: Python :: 3.10',
+        
+    ],
+)
